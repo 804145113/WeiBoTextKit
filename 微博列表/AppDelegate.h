cea43c70e021defd3c1a06e455c68f5e602ef5e8
chengjun <2846306067@qq.com>
2016-01-13 17:30:31 +0800

    init

---
+++ 微博列表/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微博列表
+//
+//  Created by GXY on 16/1/13.
+//  Copyright © 2016年 Tangxianhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微博列表/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微博列表
+//
+//  Created by GXY on 16/1/13.
+//  Copyright © 2016年 Tangxianhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
